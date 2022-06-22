c070062652b7e4326bec82482095b68e1d441abe
Shukhrat Gadoev <shukhratgadoev@77projects.com>
2022-06-22 16:33:00 +0500

    feat: -

--- api/urls.py
+++ api/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 
 urlpatterns = [
-        path('', views.getRoutes, name="routes")
+        path('', views.getRoutes, name="routes"),
+        path('notes/', views.getNotes, name="notes"),
 ]
